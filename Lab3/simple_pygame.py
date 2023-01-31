ba8ee116920ce6fa86f00f06db1935a0eafa49ff
afantino951 <andrewfa@g.ucla.edu>
2023-01-30 22:25:54 -0800

    One circle not working reverse direction

--- Lab3/simple_pygame.py
+++ Lab3/simple_pygame.py
@@ -30,7 +30,7 @@ SCREEN_HEIGHT = 600
 ITEM_WIDTH = 128
 ITEM_HEIGHT = 128
 
-CIRCLE_RADIUS = int(ITEM_WIDTH/2 + 5)
+CIRCLE_RADIUS = int(ITEM_WIDTH/2 + 2)
 
 class SpriteObject(pygame.sprite.Sprite):
     def __init__(self, x, y, img_path) -> None:
@@ -39,13 +39,11 @@ class SpriteObject(pygame.sprite.Sprite):
         self.click_surf = self.surf.copy()
         self.surf.set_colorkey((255, 255, 255), RLEACCEL)
         self.surf = pygame.transform.scale(self.surf, (ITEM_WIDTH, ITEM_HEIGHT))
-        self.rect = self.surf.get_rect(center=((SCREEN_WIDTH-(4*ITEM_WIDTH))/2, (SCREEN_HEIGHT-ITEM_HEIGHT)/2))
+        self.past_surf = self.surf.copy()
+        self.rect = self.surf.get_rect(center=(x, y))
         self.clicked = False
 
-        # self.click_surf = pygame.image.load(img_path).convert()
-        # self.click_surf.set_colorkey((255, 255, 255), RLEACCEL)
         self.click_surf = pygame.transform.scale(self.click_surf, (ITEM_WIDTH, ITEM_HEIGHT))
-
         pygame.draw.circle(self.click_surf, (255, 255, 255), (int(ITEM_WIDTH/2), int(ITEM_WIDTH/2)), CIRCLE_RADIUS, 4)
 
     def is_clicked(self):
@@ -57,23 +55,37 @@ class SpriteObject(pygame.sprite.Sprite):
                 if self.rect.collidepoint(event.pos):
                     self.clicked = not self.clicked
 
-        self.surf = self.click_surf if self.clicked else self.surf
+        if self.clicked:
+            self.surf = self.click_surf 
+        else:
+            self.surf = self.past_surf
+        
+        return self.clicked
 
 
 if __name__ == '__main__':
     # Initialize pygame
     pygame.init()
+    pygame.font.init()
 
     # Setup the clock for a decent framerate
     clock = pygame.time.Clock()
 
     # Set up the drawing window
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    my_sprite = SpriteObject(75, 75, "pygame_assets/rock.png")
-    # my_sprite = Player()
+    my_font = pygame.font.SysFont('Comic Sans MS', 50)
 
-    all_sprites = pygame.sprite.Group()
-    all_sprites.add(my_sprite)
+
+    my_sprite = SpriteObject(150, 255, "pygame_assets/rock.png")
+    paper_icon = SpriteObject(300, 255, "pygame_assets/paper.png")
+    scissor_icon = SpriteObject(450, 255, "pygame_assets/scissor.png")
+
+    rps_sprites = pygame.sprite.Group()
+    rps_sprites.add(my_sprite)
+    rps_sprites.add(paper_icon)
+    rps_sprites.add(scissor_icon)
+
+
 
     # Run until the user asks to quit
     running = True
@@ -96,13 +108,25 @@ if __name__ == '__main__':
 
         # Update the player sprite based on user keypresses
         # my_sprite.update(pressed_keys)
-        my_sprite.update(event_list)
+
+        # my_sprite.update(event_list)
 
         # Fill the background with black
         screen.fill((0, 0, 0))
+        text_surface = my_font.render('Select Rock, Paper, or Scissors', False, (255, 255, 255))
+
+        screen.blit(text_surface, (0, 100))
 
         # Draw all the sprites
-        for entity in all_sprites:
+        # NOTE: This only works if the sprites in all_sprites is a SpriteObject
+        entity_clicked = False
+        for entity in rps_sprites:
+            if not entity_clicked:
+                clicked = entity.update(event_list)
+                entity_clicked = clicked if clicked else entity_clicked
+            else:
+               entity_clicked = True 
+
             screen.blit(entity.surf, entity.rect)
         
         # Flip the display
